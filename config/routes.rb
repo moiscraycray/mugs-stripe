dc2835d1080eda77aff666c2aaf2c081b3a814fa
Olivia Mo <oliviatrademark@gmail.com>
2017-10-24 11:26:35 +1100

    generated profiles model, added validation test to profiles

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :profiles
   root 'pages#index'
 
   devise_for :users
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171023234827) do
+ActiveRecord::Schema.define(version: 20171024001945) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "profiles", force: :cascade do |t|
+    t.string "username"
+    t.text "address"
+    t.text "order_history"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -32,4 +42,5 @@ ActiveRecord::Schema.define(version: 20171023234827) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "profiles", "users"
 end
